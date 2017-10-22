0af3f979034f9c74da722e52cb61738912e37611
noodles250 <31817945+noodles250@users.noreply.github.com>
2017-10-22 21:32:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send(./views/index.js);
+  res.send('./views/index.js');
 });
 var server = app.listen(3000, function () {
   var host = server.address().address;
