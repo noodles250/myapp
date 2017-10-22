26ad71573329e422c35319e296d3c194c5dfc98d
noodles250 <31817945+noodles250@users.noreply.github.com>
2017-10-22 22:20:20 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
-app.use(express.static('public'));
 var app = express();
+app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.sendfile('./views/index.html');
 });
