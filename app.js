ea86f4b11abcb13a623b7e5180619eb47a4b978b
noodles250 <31817945+noodles250@users.noreply.github.com>
2017-10-22 21:27:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,9 @@
 var express = require('express');
-var bodyParse = require('body-parser')
+
 var app = express();
-
-app.use(express.static('static'))
-// 把前端发过来的数据用 json 解析的强制套路
-app.use(bodyParse.json())
-
-var sendHtml = function(path, response) {
-    var fs = require('fs')
-    var options = {
-        encoding: 'utf-8'
-    }
-    fs.readFile(path, options, function(err, data){
-        console.log(`读取的html文件 ${path} 内容是`, data)
-        response.send(data)
-    })
-}
-// 用 get 定义一个给用户访问的网址
-// request 是浏览器发送的请求
-// response 是我们要发给浏览器的响应
-app.get('/', function(request, response) {
-    // var r = `
-    // `
-    // fs 是 file system 文件系统的缩写
-    // fs 是 node 中处理文件和目录的库
-    // var fs = require('fs')
-    // var options = {
-    //     encoding: 'utf-8'
-    // }
-    // fs.readFile('index.html', options, function(err, data){
-    //     console.log('读取的html文件内容是', data)
-    //     response.send(data)
-    // })
-    var path = '/views/index.html'
-    var fs = require('fs')
-    var options = {
-        encoding: 'utf-8'
-    }
-    fs.readFile(path, options, function(err, data){
-        // console.log(`读取的html文件 ${path} 内容是`, data)
-        // response.send 用来发送数据给浏览器
-        response.send(data)
-    })
-})
-
+app.get('/', function (req, res) {
+  res.send('/views/index.js');
+});
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
