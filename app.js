563d2b7ec2317b2ebf178aae5962af4f7b76e2ec
noodles250 <31817945+noodles250@users.noreply.github.com>
2017-11-05 21:15:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.sendfile('./views/index.html');
 });
+app.get('/root', function (req, res) {
+  res.sendfile('root.txt');
+});
 var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
